84cdb5b0ed869d863ca8ab672b92509b3535c827
Pavle Vuković <vukovic.pavle@icloud.com>
2023-02-24 13:22:34 +0100

    update readme

--- index.ts
+++ index.ts
@@ -1,59 +1,59 @@
 /**
  * @description Generates a random numeric code with the specified number of characters
  * @export
- * @param {number} [numberOfCharacters=4]
+ * @param {number} [length=4]
  * @return {*}  {string}
  */
-export function generateNumericCode(numberOfCharacters: number = 4): string {
-  // If numberOfCharacters is less than 1, return an empty string
-  if (numberOfCharacters < 1) return "";
+export function generateNumericCode(length: number = 4): string {
+  // If length is less than 1, return an empty string
+  if (length < 1) return "";
 
-  // If numberOfCharacters is greater than 16, split the numberOfCharacters into groups of 16
-  if (numberOfCharacters > 16) {
-    const groups = Math.ceil(numberOfCharacters / 16);
-    const groupNumberOfCharacters = Math.ceil(numberOfCharacters / groups);
+  // If length is greater than 16, split the length into groups of 16
+  if (length > 16) {
+    const groups = Math.ceil(length / 16);
+    const groupLength = Math.ceil(length / groups);
     const codes = [];
     for (let i = 0; i < groups; i++) {
-      codes.push(generateNumericCode(groupNumberOfCharacters));
+      codes.push(generateNumericCode(groupLength));
     }
     return codes.join("");
   }
 
-  // Generate a random number with the specified number of numberOfCharacters
-  return Math.floor(Math.random() * 10 ** numberOfCharacters)
+  // Generate a random number with the specified number of length
+  return Math.floor(Math.random() * 10 ** length)
     .toString()
-    .padStart(numberOfCharacters, "0");
+    .padStart(length, "0");
 }
 
 /**
  * @description Generates a random alphanumeric code with the specified number of characters and letter case
  * @export
- * @param {number} [numberOfCharacters=4]
+ * @param {number} [length=4]
  * @param {("lower" | "upper" | "mixed")} [letterCase="mixed"]
  * @return {*}  {string}
  */
 export function generateAlphanumericCode(
-  numberOfCharacters: number = 4,
+  length: number = 4,
   letterCase: "lower" | "upper" | "mixed" = "mixed"
 ): string {
-  // If numberOfCharacters is less than 1, return an empty string
-  if (numberOfCharacters < 1) return "";
+  // If length is less than 1, return an empty string
+  if (length < 1) return "";
 
-  // If numberOfCharacters is greater than 16, split the numberOfCharacters into groups of 16
-  if (numberOfCharacters > 16) {
-    const groups = Math.ceil(numberOfCharacters / 16);
-    const groupNumberOfCharacters = Math.ceil(numberOfCharacters / groups);
+  // If length is greater than 16, split the length into groups of 16
+  if (length > 16) {
+    const groups = Math.ceil(length / 16);
+    const groupLength = Math.ceil(length / groups);
     const codes = [];
     for (let i = 0; i < groups; i++) {
-      codes.push(generateAlphanumericCode(groupNumberOfCharacters, letterCase));
+      codes.push(generateAlphanumericCode(groupLength, letterCase));
     }
     return codes.join("");
   }
 
-  // Generate a random string with the specified number of numberOfCharacters
+  // Generate a random string with the specified number of length
   const code = Math.random()
     .toString(36)
-    .slice(2, 2 + numberOfCharacters);
+    .slice(2, 2 + length);
 
   // If letterCase is "lower", return the code in lowercase
   if (letterCase === "lower") return code;
--- readme.md
+++ readme.md
@@ -1,3 +1,48 @@
 # Verification code
 
 This is a simple verification code generator. Supports numeric and alphanumeric verification codes.
+
+## Installation
+
+```bash
+npm install @vukovicpavle/verification-code
+```
+
+## Usage
+
+```javascript
+import {
+  generateAlphanumericCode,
+  generateNumericCode,
+} from "@vukovicpavle/verification-code";
+
+const numericCode = generateNumericCode(6);
+const alphanumericCode = generateAlphanumericCode(6, "upper");
+```
+
+## API
+
+### generateNumericCode
+
+Generates a numeric verification code.
+
+#### Parameters
+
+- `length` - The length of the code. Default is `4`.
+
+#### Returns
+
+- `string` - The generated code.
+
+### generateAlphanumericCode
+
+Generates an alphanumeric verification code.
+
+#### Parameters
+
+- `length` - The length of the code. Default is `4`.
+- `letterCase` - The case of the code. Can be `mixed`, `upper` or `lower`. Default is `mixed`.
+
+#### Returns
+
+- `string` - The generated code.
